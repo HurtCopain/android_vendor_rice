c8ed52ea887263e59cf5c2f6403bb2c5e8b5a4ce
Mathieu Chartier <mathieuc@google.com>
2022-11-28 07:36:59 +0800

    config: Speed profile services and wifi-service to reduce RAM and st…
    …orage
    
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -218,6 +218,9 @@ endif
 # Dedupe VNDK libraries with identical core variants
 TARGET_VNDK_USE_CORE_VARIANT := true
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Use a generic profile based boot image by default
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := art/build/boot/boot-image-profile.txt
